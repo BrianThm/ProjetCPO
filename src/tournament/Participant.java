e5032f8e10bf349eb8d1fdae9041e79caad14fb3
Wyzedix <wyzedix@gmail.com>
2019-05-16 19:07:01 +0200

    update participant + team

--- src/tournament/Participant.java
+++ src/tournament/Participant.java
@@ -1,6 +1,8 @@
 package tournament;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author Group
@@ -12,15 +14,18 @@ public abstract class Participant {
 	
 	private String name;
 	private Map<Game, Integer> games;
+	private Set<Tournament> tournaments;
 	
 	/**
 	 * Create a participant with a name.
 	 * @param name The participant name.
 	 */
 	public Participant(String name) {
-		assert (name != null);
+		assert name != null;
+		
 		this.name = name;
 		this.games = new HashMap<Game, Integer>();
+		this.tournaments = new HashSet<Tournament>();
 	}
 	
 	/**
@@ -30,7 +35,9 @@ public abstract class Participant {
 	 */
 	public Participant(String name, Game game) {
 		this(name);
-		games.put(game, 0);
+		assert game != null;
+		
+		this.games.put(game, 0);
 	}
 
 	/**
@@ -38,7 +45,7 @@ public abstract class Participant {
 	 * @return The name of the participant.
 	 */
 	public String getName() {
-		return name;
+		return this.name;
 	}
 	
 	/**
@@ -46,6 +53,8 @@ public abstract class Participant {
 	 * @param name The name of the participant.
 	 */
 	public void setName(String name) {
+		assert name != null;
+		
 		this.name = name;
 	}
 	
@@ -54,11 +63,13 @@ public abstract class Participant {
 	 * @param game The game played by the participant.
 	 */
 	public void plays(Game game) {
-		Integer timesPlayed = games.get(game);
+		assert game != null;
+		
+		Integer timesPlayed = this.games.get(game);
 		if (timesPlayed == null) { // never played this game before
-			games.put(game, 1);
+			this.games.put(game, 1);
 		} else {
-			games.put(game, timesPlayed + 1);
+			this.games.put(game, timesPlayed + 1);
 		}
 	}
 	
@@ -72,8 +83,8 @@ public abstract class Participant {
 		Game game = null;
 		int timesPlayed = 0;
 		
-		for (Game g : games.keySet()) {
-			int times = games.get(g);
+		for (Game g : this.games.keySet()) {
+			int times = this.games.get(g);
 			if (times >= timesPlayed) {
 				game = g;
 				timesPlayed = times;
@@ -81,5 +92,33 @@ public abstract class Participant {
 		}
 		
 		return game;
+	}
+	
+	/**
+	 * Add a tournaments played by this participant.
+	 * @param tournament The tournament played.
+	 */
+	public void addTournament(Tournament tournament) {
+		assert tournament != null;
+		
+		this.tournaments.add(tournament);
+	}
+	
+	/**
+	 * Remove a tournament played by this participant.
+	 * @param tournament The tournament to remove.
+	 */
+	public void removeTournament(Tournament tournament) {
+		assert tournament != null;
+		
+		this.tournaments.remove(tournament);
+	}
+	
+	/**
+	 * Get the tournaments played by this participant.
+	 * @return A set of tournamets played.
+	 */
+	public Set<Tournament> getTournaments() {
+		return this.tournaments;
 	}
 }
--- src/tournament/Team.java
+++ src/tournament/Team.java
@@ -1,5 +1,6 @@
 package tournament;
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Groupe
@@ -8,15 +9,86 @@ import java.util.ArrayList;
  * A team can participate to a tournament.
  */
 public class Team extends Participant {
-
-	private ArrayList<Player> members;
+	
+	private Set<Player> members;
 	
 	/**
-	 * Create a new team with a name.
+	 * Create a team with a name.
 	 * @param name The name of the team.
 	 */
 	public Team(String name) {
 		super(name);
-		members = new ArrayList<Player>();
+		this.members = new HashSet<Player>();
+	}
+	
+	/**
+	 * Create a team with a name and a base game.
+	 * @param name The name of the team.
+	 * @param game The base game of the team.
+	 */
+	public Team(String name, Game game) {
+		super(name, game);
+		this.members = new HashSet<Player>();
+	}
+	
+	/**
+	 * Add a player to the team.
+	 * @param player The new player.
+	 */
+	public void addMember(Player player) {
+		assert player != null;
+		
+		this.members.add(player);
+	}
+	
+	/**
+	 * Remove a player of the team.
+	 * @param member The player to remove.
+	 */
+	public void removeMember(Player member) {
+		assert member != null;
+		
+		this.members.remove(member);
+	}
+	
+	/**
+	 * Get the members of the team.
+	 * @return A set of the members.
+	 */
+	public Set<Player> getMembers() {
+		return this.members;
+	}
+	
+	@Override
+	/**
+	 * When a team plays a game, each member plays the game too.
+	 */
+	public void plays(Game game) {
+		super.plays(game);
+		for (Player member : this.members) {
+			member.plays(game);
+		}
+	}
+	
+	@Override
+	/**
+	 * When a team plays a tournament, each member plays the tournament too.
+	 */
+	public void addTournament(Tournament tournament) {
+		super.addTournament(tournament);
+		for (Player member : this.members) {
+			member.addTournament(tournament);
+		}
+	}
+	
+	@Override
+	/**
+	 * When a team no longer plays a tournament, each member no longer plays the tournament.
+	 */
+	public void removeTournament(Tournament tournament) {
+		super.removeTournament(tournament);
+		for (Player member : this.members) {
+			member.removeTournament(tournament);
+		}
 	}
 }
