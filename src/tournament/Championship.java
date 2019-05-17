31a284ac081cc2a1414bc2676b2c1a981b9307bf
Wyzedix <wyzedix@gmail.com>
2019-05-17 13:48:59 +0200

    championship override tournament

--- src/tournament/Championship.java
+++ src/tournament/Championship.java
@@ -15,6 +15,14 @@ public class Championship extends Tournament {
 	private Map<Participant, Integer> ranking;
 	
 	/**
+	 * Create a championship with a game.
+	 * @param game The game played in the championship.
+	 */
+	public Championship(Game game) {
+		this(game, "");
+	}
+	
+	/**
 	 * Championship constructor.
 	 * @param game The game of the tournament.
 	 * @param location The tournament location.
@@ -22,5 +30,23 @@ public class Championship extends Tournament {
 	public Championship(Game game, String location) {
 		super(game, location);
 		this.ranking = new HashMap<Participant, Integer>();
+	}
+	
+	@Override
+	/**
+	 * When a participant is added, his score is initialized at 0.
+	 */
+	public void addParticipant(Participant participant) {
+		super.addParticipant(participant);
+		this.ranking.put(participant, 0);
+	}
+	
+	@Override
+	/**
+	 * When a participant is removed, his score is removed.
+	 */
+	public void removeParticipant(Participant participant) {
+		super.removeParticipant(participant);
+		this.ranking.remove(participant);
 	}
 }
