8034164f0b1c7b74ff6dcc6467e3c3dfddf4f1ea
Enaxom <enaxom@gmail.com>
2019-05-17 14:00:08 +0200

    Ajout view

--- src/view/ViewListGame.java
+++ src/view/ViewListGame.java
@@ -1,7 +1,10 @@
 package view;
 
+import java.awt.BorderLayout;
 import java.util.Set;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import controller.Controller;
@@ -15,8 +18,24 @@ public class ViewListGame extends JPanel {
 	public ViewListGame(Controller controller, boolean deleteGame) {
 		this.controller = controller;
 		this.deleteGame = deleteGame;
-		
+
 		Set<Game> games = this.controller.getGames();
+		games.add(new Game("Game 1"));
+		games.add(new Game("Game 2"));
 		
+		JPanel list = new JPanel(new BorderLayout());
+		ImageIcon imgDelete = new ImageIcon(getClass().getResource("img/delete.png"));
+		JLabel img = new JLabel(imgDelete);
+		
+		for (Game game : games) {
+			JPanel line = new JPanel(new BorderLayout());
+			JLabel label = new JLabel(game.getName());
+			
+			list.add(line);
+			line.add(label, BorderLayout.CENTER);
+			
+			if (deleteGame)
+				line.add(img, BorderLayout.EAST);
+		}
 	}
 }
--- src/view/ViewMain.java
+++ src/view/ViewMain.java
@@ -2,6 +2,8 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -24,16 +26,43 @@ public class ViewMain extends JFrame {
 		
 		JMenuBar menubar = new JMenuBar();
 		JMenu menuGame = new JMenu("Game");
+		JMenu menuTeam = new JMenu("Team");
+		JMenu menuPlayer = new JMenu("Player");
 		JMenuItem addGame = new JMenuItem("Add a game");
 		JMenuItem deleteGame = new JMenuItem("Delete a game");
 		JMenuItem displayGames = new JMenuItem("Display all games");
 		JMenuItem manageGames = new JMenuItem("Manage games");
+		JMenuItem addTeam = new JMenuItem("Add a team");
+		JMenuItem deleteTeam = new JMenuItem("Delete a team");
+		JMenuItem displayTeams = new JMenuItem("Display all teams");
+		JMenuItem manageTeams = new JMenuItem("Manage teams");
+		JMenuItem addPlayer = new JMenuItem("Add a player");
+		JMenuItem deletePlayer = new JMenuItem("Delete a player");
+		JMenuItem displayPlayers = new JMenuItem("Display all players");
+		JMenuItem managePlayers = new JMenuItem("Manage players");
 		
 		menubar.add(menuGame);
+		menubar.add(menuTeam);
+		menubar.add(menuPlayer);
 		menuGame.add(addGame);
 		menuGame.add(deleteGame);
 		menuGame.add(displayGames);
 		menuGame.add(manageGames);
+		menuTeam.add(addTeam);
+		menuTeam.add(deleteTeam);
+		menuTeam.add(displayTeams);
+		menuTeam.add(manageTeams);
+		menuPlayer.add(addPlayer);
+		menuPlayer.add(deletePlayer);
+		menuPlayer.add(displayPlayers);
+		menuPlayer.add(managePlayers);
+		
+		displayGames.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				new ViewListGame(controller, true);
+			}
+		});
 		
 		this.setJMenuBar(menubar);
 		this.setSize(500, 500);
