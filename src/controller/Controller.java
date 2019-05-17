018de56128841574cd5ebd541ed2d4f001d9ee84
Wyzedix <wyzedix@gmail.com>
2019-05-17 11:19:03 +0200

    setter & getter du controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -17,12 +17,36 @@ public class Controller {
 		this.participants = new HashSet<Participant>();
 	}
 	
+	public void addTournament(Tournament tournament) {
+		this.tournaments.add(tournament);
+	}
+	
+	public void removeTournament(Tournament tournament) {
+		this.tournaments.remove(tournament);
+	}
+	
 	public Set<Tournament> getTournaments() {
 		return this.tournaments;
 	}
 	
+	public void addGame(Game game) {
+		this.games.add(game);
+	}
+	
+	public void removeGame(Game game) {
+		this.games.remove(game);
+	}
+	
 	public Set<Game> getGames() {
 		return this.games;
+	}
+	
+	public void addParticipant(Participant participant) {
+		this.participants.add(participant);
+	}
+	
+	public void removeParticipant(Participant participant) {
+		this.participants.remove(participant);
 	}
 	
 	public Set<Participant> getParticipants() {
