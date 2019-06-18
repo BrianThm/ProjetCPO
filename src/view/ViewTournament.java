5c920cf7d4aebb632a00be8a397a780992e3ecaa
BrianThm <thobri.85@gmail.com>
2019-06-18 16:09:34 +0200

    Modification of the View Tournament and finshing functional Part on it

--- src/view/ViewTournament.java
+++ src/view/ViewTournament.java
@@ -5,6 +5,7 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
@@ -24,6 +25,7 @@ import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
@@ -34,9 +36,11 @@ import controller.Controller;
 import tournament.Championship;
 import tournament.DoubleElimination;
 import tournament.Game;
+import tournament.Match;
 import tournament.Participant;
 import tournament.SimpleElimination;
 import tournament.Tournament;
+import tournament.exceptions.MatchDrawException;
 
 /**
  * @author group 
@@ -50,16 +54,27 @@ import tournament.Tournament;
 public class ViewTournament extends JPanel {
 
 	private Controller controller;
-	private JPanel content, panelParticipants;
+	private JPanel content; 
+	private JPanel panelParticipants;
+	private JPanel panelMatchs;
 	private JTextField textLocation;
 	private JTextField textDate;
 	private JLabel title;
 	private JTextField textTypeTournament;
 	private JTextField textGame;
+	private JTextField textWinner;
 	private JList<Participant> listParticipant;
 	private JButton tree;
 	private Tournament tournament;
 	private ViewMain fenetre;
+	private Match[] matchs;
+	private ArrayList<Match> matchsEditable;
+	private JPanel panelName;
+	private JPanel panelType;
+	private JPanel panelGame;
+	private JPanel panelButton;
+	private JPanel panelDate;
+	private JPanel panelWinner; 
 
 	/**
 	 * Constructor to create the view with the lsit of games and the form to add a game.
@@ -74,30 +89,21 @@ public class ViewTournament extends JPanel {
 		this.fenetre=fenetre;
 		this.setLayout(new BorderLayout());
 		this.content = new JPanel();
+		this.matchs = tournament.getMatchs();
+		
 		this.content.setLayout(new BoxLayout(this.content, BoxLayout.Y_AXIS));
 		
 		this.textLocation = new JTextField(20);
-		this.textLocation.setText(this.tournament.getLocation());
 		this.textLocation.setEditable(false);
 		
 		this.textDate = new JTextField(20); 		
-		SimpleDateFormat formater = new SimpleDateFormat("dd-MM-yyyy");
-		formater.format(t.getDate());
-		this.textDate.setText(""+formater);
 		this.textDate.setEditable(false);
 		
 		this.textTypeTournament = new JTextField(20);
-		if (SimpleElimination.class.isInstance(t)) {
-			textTypeTournament.setText("Simple Elimination");
-		} else if (DoubleElimination.class.isInstance(t)) {
-			textTypeTournament.setText("Double Elimination");
-		} else if (Championship.class.isInstance(t)) {
-			textTypeTournament.setText("Championship");
-		}
 		this.textTypeTournament.setEditable(false);
 		
+		
 		this.textGame= new JTextField(20);
-		this.textGame.setText(tournament.getGame().getName());
 		this.textGame.setEditable(false);
 		
 		this.panelParticipants = new JPanel(new FlowLayout());
@@ -107,27 +113,33 @@ public class ViewTournament extends JPanel {
 		this.tree.addActionListener( new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				new popup(new PanelTreeTournament(tournament.getMatchs()));
+				new popup(new PanelTreeTournament(matchs));
 			}
-		}
-				);
+		});
+		
+		this.textWinner = new JTextField(20);
+		this.textWinner.setEditable(false);
+		
+		panelMatchs = new JPanel(new GridLayout()); 
 		/* Initialization of the components */
-		JPanel panelName = new JPanel(new FlowLayout());
+	    panelName = new JPanel(new FlowLayout());
 		JLabel locationTournament = new JLabel("Location ");
 		
 		JLabel participants = new JLabel("Participants");
 		
-		JPanel panelType = new JPanel(new FlowLayout());
+		panelType = new JPanel(new FlowLayout());
 		JLabel typeTournament = new JLabel("Type ");
 		
-		JPanel panelGame = new JPanel(new FlowLayout()); 
+		panelGame = new JPanel(new FlowLayout()); 
 		JLabel gameTournament = new JLabel("Game ");
 		
-		JPanel panelDate = new JPanel(new FlowLayout()); 
+		panelDate = new JPanel(new FlowLayout()); 
 		JLabel dateTournament = new JLabel("Date ");
 		
-		JPanel panelButton = new JPanel(new FlowLayout()); 
+		panelButton = new JPanel(new FlowLayout()); 
 		
+		panelWinner = new JPanel(new FlowLayout());
+		JLabel winnerTournament = new JLabel("Winner ");
 		
 		
 		this.title = new JLabel("Tournament information");
@@ -152,19 +164,12 @@ public class ViewTournament extends JPanel {
 		
 		panelParticipants.add(participants);
 		panelParticipants.add(new JScrollPane(this.listParticipant));
-		
-		
-		content.add(title);
-		content.add(Box.createRigidArea(new Dimension(0, 20)));
-		content.add(panelName);
-		content.add(panelType); 
-		content.add(panelGame);
-		content.add(panelParticipants);
-		content.add(panelButton); 
-		tree.setSize(200, 30);
-		
-		
-		content.setBorder(BorderFactory.createEmptyBorder(15, 5, 15, 5));
+		panelWinner.add(winnerTournament); 
+		panelWinner.add(textWinner);
+				
+		matchsEditable = new ArrayList<Match>();
+		getMatchsEditable(matchsEditable, matchs, 1);
+		AdditionMatchsEditable();
 		
 		/* All is centered */
 		panelName.setAlignmentX(CENTER_ALIGNMENT);
@@ -177,6 +182,8 @@ public class ViewTournament extends JPanel {
 		this.add(content, BorderLayout.CENTER);
 		this.setAutoscrolls(true);
 		this.displayTournament();
+		this.setBorder(new CompoundBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20),
+				BorderFactory.createMatteBorder(2, 2, 2, 2, Color.gray)));
 	}
 	
 	private class popup extends JDialog{
@@ -189,10 +196,34 @@ public class ViewTournament extends JPanel {
 		}
 	}
 	
+	private void getMatchsEditable(ArrayList<Match> matchsEditable, Match[] matchList, int indice) {
+		Match m = matchList[indice];
+		if (m != null){
+			if (m.getParticipant1().getName() != "?" ) {
+				if (m.getParticipant2().getName() != "?") {
+					if (m.getWinner()==null && !m.isDraw()) {
+						matchsEditable.add(m);
+					}
+				}
+				else {
+					getMatchsEditable(matchsEditable ,matchList ,indice*2+1);
+				}
+			}
+			else {
+				getMatchsEditable(matchsEditable ,matchList ,indice*2);
+			}
+		}
+		else {
+			getMatchsEditable(matchsEditable, matchList, indice*2);
+			getMatchsEditable(matchsEditable, matchList, indice*2+1);
+		}
+	}
+	
 	private void displayTournament() {
 		clear();
 		title.setText("Tournament informations");
 		textLocation.setText(tournament.getLocation());
+		
 		if (SimpleElimination.class.isInstance(tournament)) {
 			textTypeTournament.setText("Simple Elimination");
 		} else if (DoubleElimination.class.isInstance(tournament)) {
@@ -200,21 +231,91 @@ public class ViewTournament extends JPanel {
 		} else if (Championship.class.isInstance(tournament)) {
 			textTypeTournament.setText("Championship");
 		}
+		
 		textGame.setText(tournament.getGame().getName());
+		
 		SimpleDateFormat formater = new SimpleDateFormat("dd-MM-yyyy");
 		formater.format(tournament.getDate());
 		this.textDate.setText(""+formater);
-		content.remove(panelParticipants);
+		
+		if (tournament.getWinner()!=null) {
+			this.textWinner.setText(tournament.getWinner().getName());
+		} else {
+			this.textWinner.setText("?");
+		}
+		
+		content.removeAll();
 		content.add(panelParticipants);
+		content.setBorder(BorderFactory.createEmptyBorder(15, 5, 15, 5));
+		content.add(title);
+		content.add(Box.createRigidArea(new Dimension(0, 20)));
+		content.add(panelName);
+		content.add(panelType); 
+		content.add(panelGame);
+		content.add(panelParticipants);
+		content.add(panelButton); 
+		content.add(panelWinner);
+		matchs = tournament.getMatchs();
+		getMatchsEditable(matchsEditable,matchs,1);
+		AdditionMatchsEditable();
 		refreshPanel();
 	}
 	
 	private void clear() {
+		content.removeAll();
+		matchsEditable.clear();
+		title.setText("");
 		textLocation.setText("");
 		textDate.setText("");
 		textGame.setText("");
+		textWinner.setText("");
 		textTypeTournament.setText("");
+		
 		listParticipant.clearSelection();
+	}
+	
+	private void AdditionMatchsEditable() {
+		for (Match m : matchsEditable) {
+			JPanel Match = new JPanel(new FlowLayout()); 
+			JLabel P1 = new JLabel();
+			JLabel P2 = new JLabel();
+			JTextField scorep1 = new JTextField(5);
+			scorep1.setText("0");
+			JTextField scorep2 = new JTextField(5);
+			scorep2.setText("0");
+			JButton setScore = new JButton("Submit Score");
+			setScore.addActionListener(new ActionListener(){
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					try {
+						int scoreP1 = Integer.parseInt(scorep1.getText());
+						int scoreP2 = Integer.parseInt(scorep2.getText());
+						m.setScore(scoreP1, scoreP2);
+					}catch(NumberFormatException exc) {
+						JOptionPane.showMessageDialog(content, "You must put an number in the score !",
+								"Score is not a number !", JOptionPane.ERROR_MESSAGE);
+					} catch(MatchDrawException exc1) {
+						JOptionPane.showMessageDialog(content, exc1.getMessage(),
+								"Match Draw !", JOptionPane.ERROR_MESSAGE);
+					}
+					finally {
+						clear();
+						displayTournament();
+						refreshPanel();
+					}
+				}
+			});
+			
+			P1.setText(m.getParticipant1().getName());
+			P2.setText(m.getParticipant2().getName());
+			Match.add(P1); 
+			Match.add(scorep1); 
+			Match.add(P2);
+			Match.add(scorep2); 
+			Match.add(setScore);
+			content.add(Match);			
+		}
 	}
 
 	/**
